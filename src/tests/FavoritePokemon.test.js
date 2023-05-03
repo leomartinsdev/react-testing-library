8e3abf4770b3df95a289ca498981ea58e9979504
Leonardo Martins <leonardogustavo@outlook.com>
2023-05-03 16:36:41 -0300

    Requisito 4

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FavoritePokemon from '../pages/FavoritePokemon';
-// import PokemonDetails from '../pages/PokemonDetails';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  test('A página contem um H2 escrito Page requested Not Found', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('A página contem uma IMG com a url certa', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
